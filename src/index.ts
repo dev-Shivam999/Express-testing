32afe8119d4f4f99fe5c387ddee8561b7efeada4
dev-Shivam999 <shivamswami982@gmail.com>
2024-11-15 17:21:21 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hi");
+import express from 'express';
+
+const app=express();
+
+app.use(express.json())
+
+app.post('/sum',(req, res) => {
+    const a=req.body.a
+    const b=req.body.b
+    const result = a+b
+    res.json(result)
+})
+
+app.listen(4000,() => {
+    console.log("server listening on");
+    
+})
